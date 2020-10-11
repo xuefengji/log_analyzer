43b2ad8bab183547a26854c2bc653e328b504b67
xuefengji <xuefvip@163.com>
2020-10-11 15:35:16 +0800

    feat: front page completion

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,7 +2,15 @@ const app = new Vue({
     'el': '#app',
     data() {
         return {
-            msg: 'hello,world'
+            msg: 'hello,world',
+            student:[
+                {sno: 95004, name:"张丽",gender:"女",birthday:"1992-09-09",mobile:"13587678908",email:"1354256@qq.com",address:"江苏省无锡市滨湖区",image:""},
+                {sno: 95004, name:"张丽",gender:"女",birthday:"1992-09-09",mobile:"13587678908",email:"1354256@qq.com",address:"江苏省无锡市滨湖区",image:""},
+                {sno: 95004, name:"张丽",gender:"女",birthday:"1992-09-09",mobile:"13587678908",email:"1354256@qq.com",address:"江苏省无锡市滨湖区",image:""},
+            ],
+            currentpage:1,
+            pagesize:10,
+            total:100
         }
     },
 })
