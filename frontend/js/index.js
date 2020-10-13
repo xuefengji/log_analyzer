75d638c9afa36edc961789d4fc74950ff38cf8bc
xuefengji <xuefvip@163.com>
2020-10-13 23:02:06 +0800

    feat: frontend increase query

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,6 +9,7 @@ const app = new Vue({
             currentPage:1,
             pageSize:10,
             total:0,
+            inputstr: "" //获取输入框的值
             
         }
     },
@@ -17,6 +18,28 @@ const app = new Vue({
     },
    
     methods: {
+        // 查询学生信息
+        getQueryStudent(){
+            let that = this;
+    
+            axios
+            .post(that.baseUrl + 'students/query/',{inputstr:that.inputstr})
+            .then(function(res){
+                if(res.data.code === 1){
+                    console.log(res)
+                    that.students = res.data.data;
+                    that.total = res.data.data.length;
+                    that.getPageStudent(); //获取当前页数据
+                    // console.log(that.pageStudent);
+                    // that.$message.success('查询数据成功');
+                }else{
+                    that.$message.error('查询数据错误');
+                }
+            })
+            .catch(function(err){
+                console.log(err)
+            })
+        },
         // 获取所有学生信息
         getStudents: function(){
             let that = this;
@@ -43,7 +66,7 @@ const app = new Vue({
             this.pageStudent = [];
             for(let i = (this.currentPage-1)*this.pageSize; i<this.total ;i++ ){
                 this.pageStudent.push(this.students[i]);
-                if(this.pageStudent.length === 10) break;
+                if(this.pageStudent.length === this.pageSize) break;
             }
         },
         handleSizeChange(size) {
